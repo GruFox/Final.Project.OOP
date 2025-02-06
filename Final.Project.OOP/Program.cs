a4041abeaae542fca0a7ae2895a000bfa27a1919
Gru <energimen@yandex.ru>
2025-02-06 07:32:49 +0100

    Добавьте файлы проекта.

---
+++ Final.Project.OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Final.Project.OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
